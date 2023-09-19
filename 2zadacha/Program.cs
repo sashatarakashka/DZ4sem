8e8e3d3172ff3fa12d1c1f34208c8c60b3e4e450
sashkatarakashka <lapakoza@gmail.com>
2023-09-19 03:37:15 +0300

    Решила вторую задачу

---
+++ 2zadacha/Program.cs
@@ -0,0 +1,11 @@
+﻿
+        Console.Clear();
+        Console.Write("Введите число ");
+        var n = Convert.ToInt32(Console.ReadLine());
+        var sum = 0;
+        while(n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        Console.WriteLine(sum);
