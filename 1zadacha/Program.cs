f44f4d179838e975192baa032b4d21b353d43835
sashkatarakashka <lapakoza@gmail.com>
2023-09-19 03:25:15 +0300

    Решила задачу 1

---
+++ 1zadacha/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа ");
+string text = Console.ReadLine();
+int a = Convert.ToInt32(text);
+string text2 = Console.ReadLine();
+int b = Convert.ToInt32(text2);
+double pow = Math.Pow(a,b);
+Console.WriteLine(pow);
